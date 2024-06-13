5909df3ffddeecf23178789b00ad80ec0a2e9c7c
Ankush Moundekar <moundekarankush@gmail.com>
2024-06-13 12:50:00 +0530

    next-commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import ReactDocs from "./Docs/ReactDocs/ReactDocs"
 import Installation from "./Docs/ReactDocs/Installation"
 
 function App() {
-  const path="Docs"
   return (
     <>
       <div className="main-container">
         <Routes>
-          <Route path={path+"/"} Component={Home} />
+          <Route path="/" Component={Home} />
           <Route path="/angular" Component={AngularDocs} />
           <Route path="/react" Component={ReactDocs} />
           <Route path="/react/installation" Component={Installation} />
